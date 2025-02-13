4fe1f12e40f910af08fa1725368eab1e4d431380
Amy <86963506+aeposten@users.noreply.github.com>
2025-02-12 20:59:16 -0800

    Completed more exercises

--- part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -6,7 +6,17 @@ public class AverageOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        // write your program here
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scanner.nextLine());
+
+        double average =  ((double)(num1 + num2) / 2);
+        System.out.println( "The average is " + average);
+
+        scanner.close();
 
     }
 }
--- part01-Part01_27.Ancient/src/main/java/Ancient.java
+++ part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -7,5 +7,13 @@ public class Ancient {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a year:");
+
+        int year = Integer.valueOf(scan.nextLine());
+
+        if (year < 2015) {
+            System.out.println("Ancient history!");
+        }
+        scan.close();
     }
 }
