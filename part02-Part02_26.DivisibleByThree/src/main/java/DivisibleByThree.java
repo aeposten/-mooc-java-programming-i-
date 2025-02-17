61cd31bd04da98371185dc4be9e6e917fb52c266
Amy <86963506+aeposten@users.noreply.github.com>
2025-02-17 15:54:01 -0800

    Completed more exercises

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -2,7 +2,13 @@
 import java.util.Scanner;
 
 public class DivisibleByThree {
-
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if ( i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
