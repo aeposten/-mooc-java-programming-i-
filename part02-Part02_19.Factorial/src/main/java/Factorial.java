ea15207c68646d21f3adf16e1e5f84506c1ed8fc
Amy <86963506+aeposten@users.noreply.github.com>
2025-02-17 13:46:05 -0800

    Completed more exercises

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,14 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give a number:");
+
+        int userInput = Integer.valueOf(scanner.nextLine());
+        int factorial = 1;
+
+        for (int i = factorial; i <= userInput; i++) {
+            factorial *= i;
+        }
+        System.out.println("Factorial: " + factorial);
     }
 }
--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -13,6 +13,35 @@ public class RepeatingBreakingAndRemembering {
         // the parts you haven't done, but you'll get points for the finished parts.
         
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give numbers:");
+        int sum = 0;
+        int numOfNums = 0;
+        int odd = 0;
+        int even = 0;
+
+        while (true) { 
+            int userNum = Integer.valueOf(scanner.nextLine());
+
+            if(userNum == -1) {
+                break;
+            }
+
+            if (userNum % 2 == 0) {
+                even += 1;
+            } else {
+                odd += 1;
+            }
+
+            sum += userNum;
+            numOfNums += 1;
+        }
+        double average = ((double) sum/numOfNums);
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numOfNums);
+        System.out.println("Average: " + average);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
 
     }
 }
